8664fa63938fd7e91504865811267b6430321fd9
Guryanova.Kristina <cristina.guryanova2017@yandex.ru>
2026-03-25 11:34:35 +0700

    feat: Реагирование на курсор

--- src/main.c
+++ src/main.c
@@ -29,16 +29,34 @@ static void configure_simulator()
     settings.window_height = atoi(env_h ? env_h : "128");
 }
 
-void lv_example_get_started_1(void)
-{
-    /*Change the active screen's background color*/
-    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x003a57), LV_PART_MAIN);
 
-    /*Create a white label, set its text and align it to the center*/
-    lv_obj_t * label = lv_label_create(lv_screen_active());
-    lv_label_set_text(label, "Hello world");
-    lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target_obj(e);
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Button: %d", cnt);
+    }
+}
+
+/**
+ * Create a button with a label and react on click event.
+ */
+void lv_example_get_started_2(void)
+{
+    lv_obj_t * btn = lv_button_create(lv_screen_active());     /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(label, "Button");                     /*Set the labels text*/
+    lv_obj_center(label);
 }
 
 int main()
@@ -50,7 +68,15 @@ int main()
     if(driver_backends_init_backend(selected_backend) == -1) {
         die("Failed to initialize display backend");
     }
-    lv_example_get_started_1();
+
+#if LV_USE_EVDEV
+    if(driver_backends_init_backend("EVDEV") == -1) {
+        die("Failed to initialize evdev");
+    }
+#endif
+
+    lv_example_get_started_2();
+
     driver_backends_run_loop();
 
     return 0;
